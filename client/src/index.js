cc87786a55353b1b72e63f517fd2ee6c75c0fc45
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-10-08 15:49:15 +0300

    start of client

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+//styles
+import './index.css';
+//components
+import App from './App';
+
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Route component={App} />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
